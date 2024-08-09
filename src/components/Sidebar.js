27d7ece08e70b12ff53c4ffb947ba8e7c4e508ea
hwakstar <hwakstar79@gmail.com>
2024-08-08 23:12:54 -0700

    sidebar inital

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import React,{ useState }  from 'react';
 
 const Sidebar = () => {
-  return (
-    
+  return (    
     <aside className="w-64 bg-gray-800 text-white h-screen">
       <nav className="mt-4">
         <ul>
-          <li className="py-2 px-4 hover:bg-gray-700"><a href="./home.html"> Home</a></li>
-          <li className="py-2 px-4 hover:bg-gray-700"><a href="./newemployees.html">New employee</a></li>
-          <li className="py-2 px-4 hover:bg-gray-700"><a href="./editEmployee.html">Edit and status</a></li>
+          <li className="py-2 px-4 hover:bg-gray-700"><a href="./#"> Admin Employees</a></li>
+          <li className="py-2 px-4 hover:bg-gray-700"><a href="./#"> Marketing</a></li>          
+          <li className="py-2 px-4 hover:bg-gray-700"><a href="./#"> Accounting</a></li>
+          <li className="py-2 px-4 hover:bg-gray-700"><a href="./#"> Sales</a></li>
+          <li className="py-2 px-4 hover:bg-gray-700"><a href="./#"> Logs</a></li>
         </ul>
       </nav>
-    </aside>
-  
+    </aside>  
   );
 };
 
